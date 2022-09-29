21dc5521da79a1733f483035468c3c92b407b25d
minyouki <minyokimy@gmail.com>
2022-09-29 11:38:47 +0900

    [백트래킹]9월29일

--- 9월23일-백트래킹/15663.cpp
+++ 9월23일-백트래킹/15663.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-//잚못된 코드 !!!
+
 using namespace std;
-const int SIZE = 8;
+const int SIZE = 9;
 
 int n, m;
 
@@ -12,18 +12,19 @@ vector<bool> check(SIZE);
 
 void backtracking(int cnt, vector<int> &input){ //cnt : 수열의 인덱스
 	if (cnt == m){ //길이 m인 수열 만들었다면 (기저조건)
-		for (int i = 0; i < cnt; i++){
+		for (int i = 0; i < m; i++){ //출력
 			cout << make[i] << ' ';
 		}
 		cout << '\n';
 		return ;
 	}
+	int k=0;
 	for (int i = 0; i < n; i++){
-		int num = input[i];
-		if (!check[num]){ //현재 수열에 해당 수가 없을 때만
-			make[cnt] = i; //사용
+		if (!check[i] && k != input[i]){ //현재 수열에 해당 수가 없고
+			make[cnt] = input[i]; //사용
+			k = make[cnt];
 			check[i] = true;
-			backtracking(cnt+1); //다음 인덱스 수 호출
+			backtracking(cnt+1, input); //다음 인덱스 수 호출
 			//다음 노드 탐색 시 check 배열의 해당 수 값 초기화
 			check[i] = false;
 		}
@@ -37,11 +38,11 @@ int main(){
 		cin >> input[i];
 	}
 
-	//정렬 및 중복 제거
+	//정렬
 	sort(input.begin(), input.end());
-	input.erase(unique(input.begin(), input.end()), input.end());
 
 	//연산
 	backtracking(0, input);
+
 	return 0;
 }
